c8e8cd41cda6c347690001335ce9227f78695441
Mattys <mattys.lachaise@etu.univ-nantes.fr>
2025-11-21 08:47:25 +0100

    Initial commit

---
+++ src/main/kotlin/Modeles/Pokemon.kt
@@ -0,0 +1,7 @@
+package Modeles
+
+abstract class Pokemon(nom : String, types : Array<Type>, niveau : Int) {
+    val nom : String = nom
+    val types = types
+    val niveau = niveau
+}
---
+++ src/main/kotlin/Modeles/Type.kt
@@ -0,0 +1,4 @@
+package Modeles
+
+enum class Type {
+}
