dfbdc5514657ddce88dd3a820afaecf939c1e88b
mattys <mattyslac.insta@gmail.com>
2025-12-03 16:14:33 +0100

    Début de la partie joueur

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,11 +8,21 @@ fun main() {
 
     val niveau = 50
     val pikachu = Pokemon.creer(pikachuEspece, niveau)
+    val pikachu2 = Pokemon.creer(pikachuEspece, 57)
     println(pikachu.pvActuels)
     pikachu.subirDegats(240)
     println(pikachu.pvActuels)
     println(pikachu.estKo)
-    pikachu.soinTotal()
-    println(pikachu.pvActuels)
-    println(pikachu.estKo)
+
+    val joueur1 = Joueur.creer("jean")
+    println(joueur1.nom)
+
+    joueur1.ajouterPokemon(pikachu)
+    joueur1.ajouterPokemon(pikachu2)
+    println(joueur1.equipe)
+    joueur1.supprimerPokemon(pikachu2)
+    println(joueur1.equipe)
+    joueur1.supprimerPokemon(pikachu)
+    println(joueur1.equipe)
+
 }
